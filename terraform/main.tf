b8f4829c87a152cbddcb21bad8012c9793d27d99
Andriy <andriybek@gmail.com>
2023-12-14 21:25:26 +0200

    terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,11 @@ resource "azurerm_storage_account" "sa_apps_front_end" {
   name     = "sa${var.name}${var.env}"
   location = azurerm_resource_group.rg_apps_front_end.location
 
-  account_replication_type = "LRS"
-  account_tier             = "Standard"
-  account_kind             = "StorageV2"
-  resource_group_name      = azurerm_resource_group.rg_apps_front_end.name
+  account_replication_type  = "LRS"
+  account_tier              = "Standard"
+  account_kind              = "StorageV2"
+  resource_group_name       = azurerm_resource_group.rg_apps_front_end.name
+  enable_https_traffic_only = false
 
   static_website {
     index_document     = "index.html"
